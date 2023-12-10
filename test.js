5983c1b75857bfe5c04ad8fced7ec7d80ce5e65a
DanielJDufour <daniel.j.dufour@gmail.com>
2023-12-10 12:34:01 -0500

    mvp

---
+++ test.js
@@ -0,0 +1,23 @@
+const test = require("flug");
+const unaccent = require("./unaccent.js");
+
+const cases = [
+  ["ʻĀhuimanu", "Ahuimanu"],
+  ["ʻĀinaloa", "Ainaloa"],
+  ["Haʻikū-Pauwela", "Haiku-Pauwela"],
+  ["Hāʻōʻū", "Haou"],
+  ["Hanapēpē", "Hanapepe"],
+  ["Hagåtña", "Hagatna"],
+  ["Hīlea", "Hilea"],
+  ["Hôtel de la Mer", "Hotel de la Mer"],
+  ["Mayagüez", "Mayaguez"],
+  ["San José", "San Jose"],
+  ["Utqiaġvik", "Utqiagvik"]
+];
+
+cases.forEach(([original, expected]) => {
+  test(original, ({ eq }) => {
+    const actual = unaccent(original);
+    eq(actual, expected);
+  });
+});
